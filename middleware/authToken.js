83b8497843c8c9542d6901ad0db94564d806d6f5
TheDarkLord100 <miracleyt404@gmail.com>
2024-06-23 19:42:56 +0530

    Completed Auth Route

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+    if (token == null) return res.status(401).json({ message: 'No token provided' });
 
-    verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
